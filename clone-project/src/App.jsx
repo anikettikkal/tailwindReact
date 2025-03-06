ca1256cda4279a610e3c3d2f2972f193f283e7c6
anikettikkal <tikkalaniket2019@gmail.com>
2025-03-07 02:35:57 +0530

    navbar completed

--- clone-project/src/App.jsx
+++ clone-project/src/App.jsx
@@ -7,7 +7,23 @@ function App() {
 
   return (
     <>
-       <h1>aniket</h1>
+    {/* navbar starts here*/}
+    <nav className='w-full h-14 bg-indigo-200 flex
+    px-4 md:px-4 items-center justify-between'>
+      <div className='text-2xl text-indigo-700
+      font-bold'>PW skills</div>
+      <ul className='md:flex hidden font-semibold'>
+        <li className='mx-[10px] cursor-pointer'>Home</li>
+        <li className='mx-[10px] cursor-pointer'>About-Us</li>
+        <li className='mx-[10px] cursor-pointer'>Contact-Us</li>
+      </ul>
+      <div className='hidden md:block px-2 py2 bg-indigo-700 text-white rounded
+      font-bold cursor-pointer'>Login/Signup</div>
+      <div className='md:hidden'>
+        <a className='text-4xl' href="#">&#8801;</a>
+      </div>
+    </nav>
+    {/* navbar ends here */}
     </>
   )
 }
